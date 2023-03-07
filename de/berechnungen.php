d940f50694514b03b344959b49665eaeaee80c7b
DochSergeantTV <schlesiger.j@web.de>
2023-03-07 15:54:50 +0100

    Calculator Done

--- de/berechnungen.php
+++ de/berechnungen.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require "structure/header.php";
 ?>
 
@@ -6,69 +6,198 @@ require "structure/header.php";
 
 <body>
 
-<nav class="nv navbar navbar-expand-lg bg-body-tertiary shadow">
-    <div class="container-fluid ps-4">
-        <a class="navbar-brand mb-auto nv-brand" href="index.php">SpaceMiner</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="dashboard.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="auftraege.php">Aufträge</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="berechnungen.php">Berechnungen</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="uebermich.php">Über Mich</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="registrieren.php">Registrieren</a>
-                </li>
-                <li class="nav-item me-2">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
+    <nav class="nv navbar navbar-expand-lg bg-body-tertiary shadow">
+        <div class="container-fluid ps-4">
+            <a class="navbar-brand mb-auto nv-brand" href="index.php">SpaceMiner</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="auftraege.php">Aufträge</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="berechnungen.php">Berechnungen</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="uebermich.php">Über Mich</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registrieren.php">Registrieren</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
 
-                <form class="d-flex" role="search">
-                    <button class="btn btn-outline-success mt-2 mb-2" type="submit"
-                        style="--bs-btn-padding-y: .2rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">DE</button>
+                    <form class="d-flex" role="search">
+                        <button class="btn btn-outline-success mt-2 mb-2" type="submit" style="--bs-btn-padding-y: .2rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">DE</button>
+                    </form>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="calculate">
+        <div class="calculate_sub card">
+            <h5 class="header-text card-header text-center">Berechne den Wert eines Steins</h5>
+            <div class="card-body">
+
+
+                <form action="">
+                    <div class="mt-2 row ">
+                    <div class="col-lg-1"></div>
+                        <div class="col-lg-2">Masse des Steins:</div>
+                        <div class="col-lg-2">
+                            <input type="text" id="massStone" class="form-control">
+                        </div>
+                        <div class="col-lg-2 job-addcrew helptext">
+                            <span>
+                                <i>Zum Beispiel 6900</i>
+                            </span>
+                        </div>
+                        <div class="col-lg-2"></div>
+                        <div class="col-lg-2"></div>
+                    </div>
+
+                    <hr>
+
+                    <div class="mt-4 row justify-content-center">
+                        <div class="col-lg-1">
+                        </div>
+                        <div class="col-lg-2">Erztyp:</div>
+                        <div class="col-lg-2">Anteil (%):</div>
+                        <div class="col-lg-2">Menge (SCU):</div>
+                        <div class="col-lg-2">Preis (ROH):</div>
+                        <div class="col-lg-2">Preis (BESSER):</div>
+                        <div class="col-lg-1"></div>
+                    </div>
+                    <div class="mt-2 row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2">
+                            <div class="col-3 text-info calculateOre">
+                                <select class="form-select">
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <input type="text" id="proportion" class="form-control">
+                        </div>
+                        <div class="col-lg-2 ">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-1"></div>
+                    </div>
+                    <div class="mt-4 row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2">
+                            <div class="col-3 text-info calculateOre">
+                                <select class="form-select">
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <input type="text" id="proportion" class="form-control">
+                        </div>
+                        <div class="col-lg-2 ">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-1"></div>
+                    </div>
+                    <div class="mt-4 row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2">
+                            <div class="col-3 text-info calculateOre">
+                                <select class="form-select">
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <input type="text" id="proportion" class="form-control">
+                        </div>
+                        <div class="col-lg-2 ">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-2">
+                            <input class="form-control" type="text" value="" aria-label="Disabled input example" disabled readonly>
+                        </div>
+                        <div class="col-lg-1"></div>
+                    </div>
+
+                    <hr>
+
+                    <div class="mt-4 row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2 text-center"><i class="fas fa-weight-hanging"></i> Steingewicht</div>
+                        <div class="col-lg-2 text-center"><i class="fa-regular fa-gem"></i> Mineralisches Gewicht</div>
+                        <div class="col-lg-2 text-center"><i class="fa-regular fa-trash-can"></i> Müllgewicht</div>
+                        <div class="col-lg-2 text-center"><i class="fa-regular fa-circle-check"></i> Rohgewinn</div>
+                        <div class="col-lg-2 text-center"><i class="fas fa-users-cog"></i> Aufgearbeiteter Gewinn</div>
+                        <div class="col-lg-1"></div>
+                    </div>
+                    <div class="mt-2 row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2 h5 text-center">0 cSCU</div>
+                        <div class="col-lg-2 h5 text-center">0 cSCU</div>
+                        <div class="col-lg-2 h5 text-center text-warning">0 cSCU</div>
+                        <div class="col-lg-2 h5 text-center text-success">0 aUEC</div>
+                        <div class="col-lg-2 h5 text-center text-success">0 aUEC</div>
+                        <div class="col-lg-1"></div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-2 h6 text-center">0 SCU</div>
+                        <div class="col-lg-2 h6 text-center">0 SCU</div>
+                        <div class="col-lg-2 h6 text-center text-warning">0 SCU</div>
+                        <div class="col-lg-2 h6 text-center text-success"></div>
+                        <div class="col-lg-2 h6 text-center text-success"></div>
+                        <div class="col-lg-1"></div>
+                    </div>
+
                 </form>
 
-            </ul>
+            </div>
         </div>
+
     </div>
-</nav>
 
 
-<div class="calculate">
-    <div class="calculate_sub card">
-        <h5 class="header-text card-header text-center">Berechne den Wert eines Steins</h5>
-        <div class="card-body">
-
-
-            <form action="">
-                <div class="row justify-content-center">
-                    <div class="col">Masse des Steins:</div>
-                </div>
-            </form>
-
-        </div>
-    </div>
-                
-</div>
-
-
-<?php
+    <?php
     require "structure/footer.php";
-?>
+    ?>
 
-   
+
 </body>
+
 </html>
