14d04a968c538735c4175106f224b7567974eab4
DochSergeantTV <schlesiger.j@web.de>
2023-03-16 15:50:21 +0100

    save

---
+++ de/functions/register_query.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+function username(){
+    if(isset($_POST["username"])){
+        if($_POST["username"] == ""){
+            return "Nutzername darf nicht leer sein";
+        }
+    }
+}
+
+function password(){
+    if(isset($_POST["password"])){
+        if($_POST["password"] == ""){
+            return "Password darf nicht leer sein";
+        }
+    }
+    if(isset($_POST["password2"])){
+        if($_POST["password2"] == ""){
+            return "Wiederholung darf nicht leer sein";
+        }
+    }
+    if(isset($_POST["password"], $_POST["password2"])){
+        if ($_POST["password"] != $_POST["password2"]){
+            return "Password ungleich";
+        }
+    }
+}
+
+
+
+
+?>
