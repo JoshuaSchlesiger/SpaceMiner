a436b3f26b5ad1fa942b33fe49b2664615765af1
DochSergeantTV <schlesiger.j@web.de>
2023-03-19 10:29:34 +0100

    repair

--- de/structure/footer.php
+++ de/structure/footer.php
@@ -141,6 +141,11 @@
   <!-- Copyright -->
 </footer>
 
+<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="scripts/colormode.js"></script>
+
+
 <script type="module" src="https://cookieconsent.popupsmart.com/js/CookieConsent.js" ></script>
 <script type="text/javascript" src="https://cookieconsent.popupsmart.com/js/App.js"></script>
 <script>
