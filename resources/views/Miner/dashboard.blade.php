7da54a206ec6ad9c3a303e76663ff494e57de36b
Joshua Schlesiger <schlesiger.j@web.de>
2023-11-26 16:16:21 +0100

    Dashboard design

--- resources/views/Miner/dashboard.blade.php
+++ resources/views/Miner/dashboard.blade.php
@@ -10,56 +10,194 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <div class="header-text card-header">
-                        <div class="text-center fs-4">
-                            Fertige Aufträge
+                    <div>
+                        <div class="header-text card-header">
+                            <div class="text-center fs-4">
+                                Fertige Aufträge
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body finishedTaskList">
-                        <div class="row listItems align-items-center ms-2 me-2">
-                            <div class="col-4 fs-5 d-flex justify-content-evenly">
-                                <div class="text-white-50">Station:</div>
-                                <div class="ms-2 text-info text-center">ARC-L2</div>
-                            </div>
-                            <div class="col-3 fs-5 d-flex justify-content-evenly">
-                                <div class="text-white-50">Spieleranzahl:</div>
-                                <div class="ms-2 text-info">4</div>
-                            </div>
-                            <div class="col-5 fs-5 d-flex justify-content-evenly">
-                                <div class="text-white-50">Erze:</div>
-                                <div class="ms-2 text-info">
-                                    <select class="form-select text-center form-select-sm">
-                                        <option value="1">
-                                            Quantianium: 2022
-                                        </option>
-                                    </select>
+                        <div class="card-body finishedTaskList">
+                            <div class="row listItems align-items-center ms-2 me-2">
+                                <div class="col-4 fs-5 d-flex justify-content-evenly">
+                                    <div class="text-white-50">Station:</div>
+                                    <div class="ms-2 text-info text-center">ARC-L2</div>
+                                </div>
+                                <div class="col-3 fs-5 d-flex justify-content-evenly">
+                                    <div class="text-white-50">Spieleranzahl:</div>
+                                    <div class="ms-2 text-info">4</div>
+                                </div>
+                                <div class="col-5 fs-5 d-flex justify-content-evenly">
+                                    <div class="text-white-50">Erze:</div>
+                                    <div class="ms-2">
+                                        <select class="form-select text-center form-select-sm">
+                                            <option value="1">
+                                                Quantianium: 2022
+                                            </option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
+                            <hr>
                         </div>
-                        <hr>
                     </div>
+
+                    {{-- <div>
+                        <div class="header-text card-header text-center fs-4">
+                            <div class="text-center fs-4 my-auto">
+                                Spieler bezahlen
+                            </div>
+                        </div>
+                        <div class="card-body">
+
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Spielername:</div>
+                                </div>
+                                <div class="col-lg-4 col-6 fs-4 text-info">
+                                    DochSergeantTV
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Gesamtbetrag:</div>
+                                </div>
+                                <div class="col-lg-4 col-6">
+                                    <div class="ms-2 fs-4 text-center">
+                                        <span class="text-danger">23422</span> <span class="text-white-50">aUEC</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Teilzahlung:</div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="ms-2">
+                                        <select class="form-select text-center">
+                                            <option value="" class="" hidden selected disabled>
+                                                Bitte wählen</option>
+                                            <option value="1">
+                                               234234 aUEC
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1 col-6">
+                                    <button type="button" class="btn btn-outline-warning btn-sm">Teilzahlen</button>
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-evenly mt-4">
+                                <button type="button" class="btn btn-outline-success btn-lg">Gesamtzahlung</button>
+                                <button type="button" class="btn btn-outline-danger btn-lg">Abbrechen</button>
+                            </div>
+                        </div>
+                    </div> --}}
                 </div>
             </div>
 
-            <div class="col">
+            <div class="col ">
                 <div class="card">
-                    <div class="header-text card-header text-center fs-4">
-                        Zu bezahlende Spieler
-                    </div>
-                    <div class="card-body payablePlayerList">
+                    <div>
+                        <div class="header-text card-header text-center fs-4">
+                            Zu bezahlende Spieler
+                        </div>
+                        <div class="card-body payablePlayerList">
 
-                        <div class="row listItems align-items-center ms-2 me-2">
-                            <div class="col fs-5 d-flex justify-content-evenly">
-                                <div class="text-white-50">Spielername:</div>
-                                <div class="ms-2 text-info text-center">DochSergeantTV</div>
+                            <div class="row listItems align-items-center ms-2 me-2">
+                                <div class="col fs-5 d-flex justify-content-evenly">
+                                    <div class="text-white-50">Spielername:</div>
+                                    <div class="ms-2 text-info text-center">DochSergeantTV</div>
+                                </div>
+                                <div class="col fs-5 d-flex justify-content-evenly">
+                                    <div class="text-white-50">Betrag:</div>
+                                    <div><span class="text-danger">234</span><span> aUEC</span></div>
+                                </div>
                             </div>
-                            <div class="col fs-5 d-flex justify-content-evenly">
-                                <div class="text-white-50">Betrag:</div>
-                                <div><span class="text-danger">234</span><span> aUEC</span></div>
+                            <hr>
+                        </div>
+                    </div>
+
+
+                    {{-- <div>
+                        <div class="header-text card-header text-center fs-4">
+                            <div class="row">
+                                <div class="col-9 text-center fs-4 my-auto">
+                                    Verkaufen
+                                </div>
+                                <div class="col-3 d-flex justify-content-end">
+                                    <button type="button" class="btn btn-outline-warning">Kombinieren</button>
+                                </div>
                             </div>
                         </div>
-                        <hr>
-                    </div>
+                        <div class="card-body">
+
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Erzmetall:</div>
+                                </div>
+                                <div class="col-lg-4 col-6">
+                                    <div class="ms-2">
+                                        <select class="form-select text-center">
+                                            <option value="1">
+                                                Quantianium
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Einheiten:</div>
+                                </div>
+                                <div class="col-lg-4 col-6">
+                                    <div class="ms-2 fs-4 text-center">
+                                        <span>2342</span> <span class="text-white-50">units</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Verkaufsstation:</div>
+                                </div>
+                                <div class="col-lg-4 col-6">
+                                    <div class="ms-2">
+                                        <select class="form-select text-center">
+                                            <option value="1">
+                                                Area18
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row align-items-center ms-2 me-2 mt-2">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-4 col-6 fs-4">
+                                    <div class="text-white-50">Verkaufspreis:</div>
+                                </div>
+                                <div class="col-lg-4 col-6">
+                                    <div class="ms-2 fs-4">
+                                        <input id="massStone" type="text" class="form-control text-center"
+                                            placeholder="69420 aUEC" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-evenly mt-3">
+                                <button type="button" class="btn btn-outline-success btn-lg">Speichern</button>
+                                <button type="button" class="btn btn-outline-danger btn-lg">Abbrechen</button>
+                            </div>
+                        </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
@@ -92,13 +230,14 @@
                             <div class="ms-2 ">4</div>
                         </div>
                         <div class="col fs-5 d-flex justify-content-evenly">
-                            <div class="text-white-50">Einlagerung am:</div>
-                            <div class="ms-2 ">10-10-2023</div>
+                            <div class="text-white-50">Fertig am:</div>
+                            <div class="ms-2 ">10.10.2023 22:20</div>
                         </div>
                     </div>
                     <div class="mt-3 ms-5 me-4">
                         <div class="progress">
-                            <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 25%"
+                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     </div>
                 </div>
