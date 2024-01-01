db1bb00281faa29ab1ec8e8501e69ca5aa2d724c
Joshua Schlesiger <schlesiger.j@web.de>
2024-01-01 13:23:52 +0100

    Update title and description

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="Miningtool for Star Citizen">
+    <meta name="description" content="Mining tool for Star Citizen that remembers how much you have to pay your crew">
     <link rel=“canonical“ href=“https://www.seite.de/dieine/“/>
     <meta name="author" content="DochSergeantTV">
-    <title>SpaceMiner for Star Citizen</title>
+    <title>SpaceMiner | Calculate stone values and save orders</title>
 
         <!-- Scripts -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/welcome.css', 'resources/css/calculator.css', 'resources/css/task.css', 'resources/css/dashboard.css'])
