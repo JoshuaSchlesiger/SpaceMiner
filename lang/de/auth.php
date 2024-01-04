8cc059aa7494f45644d96ec368f289668139bf7e
jschlesiger <joshua.schlesiger@steep.de>
2024-01-04 13:15:13 +0100

    Transl Auth

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -16,4 +16,31 @@ return [
     'failed' => 'Diese Anmeldedaten stimmen nicht mit unseren Aufzeichnungen überein',
     'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es erneut in :seconds Sekunden.',
 
+    'view.login.header' => 'Anmelden',
+    'view.login.email' => 'E-Mail',
+    'view.login.password' => 'Passwort',
+    'view.login.rememberMe' => 'Anmeldedaten merken',
+    'view.login.login' => 'Anmelden',
+    'view.login.forgetPassword' => 'Passwort vergessen?',
+
+    'view.register.header' => 'Registrieren',
+    'view.register.username' => 'Nutzername',
+    'view.register.email' => 'E-Mail',
+    'view.register.password' => 'Passwort',
+    'view.register.passwordConfirm' => 'Passwort bestätigen',
+    'view.register.register' => 'Registrieren',
+
+    'view.passwordReset.header' => 'Passwort zurücksetzen',
+    'view.passwordReset.send' => 'Link zum Zurücksetzen des Passworts senden',
+    'view.passwordReset.email' => 'E-Mail-Adresse',
+    'view.passwordReset.password' => 'Passwort',
+    'view.passwordReset.passwordConfirm' => 'Passwort bestätigen',
+    'view.passwordReset.resetPassword' => 'Passwort zurücksetzen',
+
+    'view.passwordConfirm.header' => 'Passwort bestätigen',
+    'view.passwordConfirm.text' => 'Bitte bestätigen Sie Ihr Passwort, bevor Sie fortfahren.',
+    'view.passwordConfirm.password' => 'Passwort',
+    'view.passwordConfirm.passwordConfirm' => 'Passwort bestätigen',
+    'view.passwordConfirm.forgetPassword' => 'Passwort vergessen?',
+
 ];
--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,4 +16,31 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'view.login.header' => 'Login',
+    'view.login.email' => 'Email Address',
+    'view.login.password' => 'Password',
+    'view.login.rememberMe' => 'Remember Me',
+    'view.login.login' => 'Login',
+    'view.login.forgetPassword' => 'Forgot your password?',
+
+    'view.register.header' => 'Register',
+    'view.register.username' => 'Username',
+    'view.register.email' => 'Email Address',
+    'view.register.password' => 'Password',
+    'view.register.passwordConfirm' => 'Confirm password',
+    'view.register.register' => 'Register',
+
+    'view.passwordReset.header' => 'Reset password',
+    'view.passwordReset.send' => 'Send Password Reset Link',
+    'view.passwordReset.email' => 'Email Address',
+    'view.passwordReset.password' => 'Password',
+    'view.passwordReset.passwordConfirm' => 'Confirm password',
+    'view.passwordReset.resetPassword' => 'Reset password',
+
+    'view.passwordConfirm.header' => 'Confirm password',
+    'view.passwordConfirm.text' => 'Please confirm your password before continuing.',
+    'view.passwordConfirm.password' => 'Password',
+    'view.passwordConfirm.passwordConfirm' => 'Confirm password',
+    'view.passwordConfirm.forgetPassword' => 'Forgot your password?',
+
 ];
