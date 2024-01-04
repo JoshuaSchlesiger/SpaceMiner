7624d410301d4171bd4e0205dbea766487845d83
jschlesiger <joshua.schlesiger@steep.de>
2024-01-04 12:10:04 +0100

    Ger Trans userSettings

---
+++ lang/de/userSettings.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'view.header' => 'Benutzereinstellungen',
+    'view.visibilityExternalTasks' => 'Aufträge von Anderen sehen',
+    'view.whitelistedPlayer' => 'Spieler auf der Whitelist',
+    'view.name' => 'Name',
+];
