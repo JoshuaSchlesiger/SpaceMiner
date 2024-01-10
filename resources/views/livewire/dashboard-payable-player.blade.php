ed53a5972e99cf436b6a515744d040f5ddcd45b4
Joshua Schlesiger <schlesiger.j@web.de>
2024-01-10 17:05:47 +0100

    testing

--- resources/views/livewire/dashboard-payable-player.blade.php
+++ resources/views/livewire/dashboard-payable-player.blade.php
@@ -172,7 +172,8 @@
                 </div>
                 <div class="card-body payablePlayerListShared">
                     @foreach ($taskOfOtherUsers as $id => $info)
-                        <div class="row listItems align-items-center ms-2 me-2"
+                        <?php var_dump($info) ?>
+                        {{-- <div class="row listItems align-items-center ms-2 me-2"
                             wire:click.prevent='showModal({{ $id }}, "payablePlayer2")'>
                             <div class="col row fs-5 d-flex justify-content-evenly">
                                 <div class="col-6 text-white-50 text-center">@lang('dashboard.view.payablePlayer.creatorName'):</div>
@@ -185,7 +186,7 @@
                                         <span>aUEC</span>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                         <hr>
                     @endforeach
                 </div>
