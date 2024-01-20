489223094b593c5c15034a93003b8aa245c1c784
Joshua Schlesiger <schlesiger.j@web.de>
2024-01-20 14:23:26 +0100

    Lang update

--- lang/de/aboutMe.php
+++ lang/de/aboutMe.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'view.cardTitle' => 'Who am I?',
-    'view.cardTextTitle' => 'I am Joshua (DochSergeantTV / MilschSchnitte)',
+    'view.cardTitle' => 'Wer bin ich?',
+    'view.cardTextTitle' => 'Ich bin Joshua (DochSergeantTV / MilschSchnitte)',
     'view.cardText' => "Ein so typischer Softwareentwickler, der nicht viel vom Leben bisher weiß. Aktuell habe ich einfach Lust und Langeweile während meiner Ausbildung diese doch ziemlich coole Website für Star Citizen zu bauen.
                         <br><br>
                         Ich helfe gerne anderen Mitspieler oder Spiele mit ein paar großen oder kleinen Gruppen eine Runde Mining im All. Wenn du mal jemanden wie da rechts im Bild mit einer pinken Katzenrüstung rumlaufen siehts, da bin das bestimmt ich oder ein anderer doch auch so verblödeter Depp. Es kann auch sein, das du mal wen gegen eine Station fliegen siehts mit seiner Prosi. Das bin dann auch ich gewesen.
