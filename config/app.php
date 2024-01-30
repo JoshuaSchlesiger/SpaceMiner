f6e4cda8116e67ba187cb4bbd1fface8648c13f5
Joshua Schlesiger <schlesiger.j@web.de>
2024-01-30 19:16:54 +0100

    revert

--- config/app.php
+++ config/app.php
@@ -169,7 +169,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CookiesServiceProvider::class,
-        andcarpi\Popper\PopperServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,7 +184,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Popper' => andcarpi\Popper\Facades\Popper::class,
     ])->toArray(),
 
 ];
