ffef743540c74a53a2f39644f9c866ed587de1dc
Joshua Schlesiger <schlesiger.j@web.de>
2024-01-31 17:47:17 +0100

    seo 3

--- lang/en/welcome.php
+++ lang/en/welcome.php
@@ -6,11 +6,11 @@ return [
     'view.subHeader' => 'This is where the fun begins, trust me :D',
     'view.cardTitle' => 'What am I?',
     'view.cardTextTitle' => 'SpaceMiner: Your expert in interstellar resources',
-    'view.cardText' => "Are you tired of being in the dark about the value of a space rock or planet-side mineral? Worried that a particular rock might exceed your ship's mass capacity?
+    'view.cardText' => "Are you tired of being in the dark about the value of a space stone or planet-side mineral? Worried that a particular stone might exceed your ship's mass capacity?
     <br><br>
-    When gearing up for an extensive mining expedition with your crew or organization, you may face the challenge of tracking who mined what, where, and how much, followed by the tedious task of transferring resources to the respective players.
+    When gearing up for an extensive mining expedition in Star Citizen with your crew or organization, you may face the challenge of tracking who mined what, where, and how much, followed by the tedious task of transferring resources to the respective players.
     <br>
-    Enter SpaceMiner – your solution to these challenges. Our website not only provides precise calculations for the value of space rocks but also stores this data for your convenience.
+    Enter SpaceMiner – your solution to these challenges. Our website not only provides precise calculations for the value of space stones but also stores this data for your convenience.
     <br><br>
     Sign up now to unlock the full potential of SpaceMiner. Beyond accurate resource values, you can create assignments for your crew, track completion times, monitor individual contributions, and easily compensate participants.
     <br>
